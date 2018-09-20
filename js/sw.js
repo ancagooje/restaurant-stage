fb9d8863d636d3cec610668c06678ce32aa2b3e9
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-09-20 14:09:11 -0400

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,46 @@
+self.addEventListener('install', function(event) {
+    // Perform install steps
+  });
+  var CACHE_NAME = 'my-site-cache-v1';
+  var urlsToCache = [
+    '/',
+    '/styles/main.css',
+    '/script/main.js'
+  ];
+  
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+ console.log('Service Worker: Registered');
+ 
+ self.addEventListener('install', function(e) {
+
+ });
+
+ const cacheFiles = [
+     '/',
+     '/index.html',
+     '/restaurant.html',
+     '/css/styles.css',
+     '/js/dbhelper.js',
+     '/js/main.js',
+     '/js/restaurant_info.js',
+     '/data/restaurants.json',
+     '/img/1.jpg',
+     '/img/2.jpg',
+     '/img/3.jpg',
+     '/img/4.jpg',
+     '/img/5.jpg',
+     '/img/6.jpg',
+     '/img/7.jpg',
+     '/img/8.jpg',
+     '/img/9.jpg',
+     '/img/10.jpg'
+ ];
