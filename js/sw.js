b0fa8d052fc4bb9e668da7db3197ee6cdb58b050
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-09-22 13:25:49 -0400

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,19 @@
+/** Service worker example code from https://developers.google.com/web/fundamentals/primers/service-workers/ **/
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
 self.addEventListener('install', function(event) {
     // Perform install steps
   });
