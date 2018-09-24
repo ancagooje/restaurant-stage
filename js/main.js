19893d4c2584af28861db0e73a84eb22b0498cc6
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-09-24 11:29:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,19 +19,6 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
