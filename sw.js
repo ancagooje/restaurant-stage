2e52e50f0fa6a8773843eecad53b60fb174ab952
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-09-24 11:54:28 -0400

    Update sw.js
    Corrected the service worker sw.js file

--- sw.js
+++ sw.js
@@ -1,63 +1,52 @@
-/**Service worker example code from https://developers.google.com/web/fundamentals/primers/service-workers/ */
 
+ self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("restaurant-static-v2").then(cache => {
+      return cache.addAll([
+        "./",
+        "./img/1.jpg",
+        "./img/2.jpg",
+        "./img/3.jpg",
+        "./img/4.jpg",
+        "./img/5.jpg",
+        "./img/6.jpg",
+        "./img/7.jpg",
+        "./img/8.jpg",
+        "./img/9.jpg",
+        "./img/10.jpg",
+        "./index.html",
+        "./restaurant.html",
+        "./css/styles.css",
+        "./data/restaurants.json",
+        "./js/dbhelper.js",
+        "./js/main.js",
+        "./js/restaurant_info.js" 
+      ]);
+    })
+  );
+});
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(cacheName => {
+            return cacheName.startsWith("restaurant-") &&
+              cacheName !== "restaurant-static-v1";
+          }).map(cacheName => {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+  );
+});
 
-
-
-self.addEventListener('install', function(event) {
-    // Perform install steps
-  });
-  var CACHE_NAME = 'my-site-cache-v1';
-  var urlsToCache = [
-    '/',
-    '/styles/main.css',
-    '/script/main.js'
-  ];
-  
-  self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
- console.log('Service Worker: Registered');
- 
- self.addEventListener('install', function(e) {
-
- });
-
- const cacheFiles = [
-     '/',
-     '/index.html',
-     '/restaurant.html',
-     '/css/styles.css',
-     '/js/dbhelper.js',
-     '/js/main.js',
-     '/js/restaurant_info.js',
-     '/data/restaurants.json',
-     '/img/1.jpg',
-     '/img/2.jpg',
-     '/img/3.jpg',
-     '/img/4.jpg',
-     '/img/5.jpg',
-     '/img/6.jpg',
-     '/img/7.jpg',
-     '/img/8.jpg',
-     '/img/9.jpg',
-     '/img/10.jpg'
- ];
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
