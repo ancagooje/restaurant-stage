a22705e5fc8e555e3097cd7e94a35de3bacf3bd9
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-09-25 15:07:49 -0400

    Update sw.js
    Removed "activate" listener as it was emptying the cache unnecessarily.

--- sw.js
+++ sw.js
@@ -26,22 +26,6 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.filter(cacheName => {
-            return cacheName.startsWith("restaurant-") &&
-              cacheName !== "restaurant-static-v1";
-          }).map(cacheName => {
-            return caches.delete(cacheName);
-          })
-        );
-      })
-  );
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
